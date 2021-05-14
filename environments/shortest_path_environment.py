fixed oracle policy value calculation

environments/shortest_path_environment.py
@@ -34,9 +34,9 @@ class ShortestPathEnvironment(AbstractEnvironment):
         """
         x = torch.randn(n, self.context_dim)
         m = self.decision_dim
-        poly_x = torch.from_numpy(self.poly.fit_transform(x)).float()
+        y_mean = self.compute_oracle_mean_y(x)
         noise = 0.75 + 0.5 * torch.rand(n, m)
-        y = (poly_x @ self.w.T + 3) * noise
+        y = y_mean * noise
         return x, y
 
     def _encode_coord(self, x, y):
@@ -92,3 +92,7 @@ class ShortestPathEnvironment(AbstractEnvironment):
         :return: decision_dim for given environment
         """
         return self.decision_dim
+
+    def compute_oracle_mean_y(self, x):
+        poly_x = torch.from_numpy(self.poly.fit_transform(x)).float()
+        return poly_x @ self.w.T + 3

environments/toy_environment.py
@@ -26,8 +26,7 @@ class ToyEnvironment(AbstractEnvironment):
             and y is of shape (n, decision_dim)
         """
         x = torch.randn(n, self.context_dim)
-        h = softplus(x @ self.theta)
-        y_mean = h.reshape(-1, 1) + self.y_0.reshape(1, -1)
+        y_mean = self.compute_oracle_mean_y(x)
         y = y_mean + torch.randn(y_mean.shape) * self.y_sigma
         return x, y
 
@@ -50,3 +49,7 @@ class ToyEnvironment(AbstractEnvironment):
         :return: decision_dim for given environment
         """
         return self.decision_dim
+
+    def compute_oracle_mean_y(self, x):
+        h = softplus(x @ self.theta)
+        return h.reshape(-1, 1) + self.y_0.reshape(1, -1)

optimization/dummy_lp_solver.py
@@ -3,7 +3,7 @@ from torch.nn.functional import one_hot
 
 
 class DummyLPSolver(object):
-    def __init__(self, constraints, context_dim, decision_dim):
+    def __init__(self, constraints, context_dim, decision_dim, batch_size=None):
         """
         :param constraints: general constraints on feasible Z for LP
         :param context_dim: dimensionality of context (x)
@@ -12,6 +12,7 @@ class DummyLPSolver(object):
         self.constraints = constraints
         self.context_dim = context_dim
         self.decision_dim = decision_dim
+        self.batch_size = batch_size
 
     def solve_lp(self, y):
         """